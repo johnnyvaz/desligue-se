438f9d9bb76bee6f850f2667c5ba5464264ad74a
johnnyvaz1 <johnny.ferreira@luizalabs.com>
2022-07-28 19:08:55 -0300

    ci: init

---
+++ app/pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="home">
+    <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
+        <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
+
+        <div class="mb-12 xl:mb-0">
+          <a href="https://t.me/desligue_se">https://t.me/desligue_se</a>
+        </div>
+      </div>
+      <div class="flex flex-col w-full xl:w-2/5">
+        <img
+          alt="Hero"
+          class="rounded shadow-xl"
+          src="https://source.unsplash.com/random/720x400"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import settings from '@/content/settings/general.json';
+
+@Component({
+  // Called to know which transition to apply
+  transition() {
+    return 'slide-left';
+  },
+})
+export default class Home extends Vue {
+  welcomeText = settings.welcomeText;
+
+  get posts(): Post[] {
+    return this.$store.state.posts;
+  }
+
+  isSignedUp = false;
+
+  form = {
+    email: '',
+  };
+
+  encode(data): string {
+    return Object.keys(data)
+      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&');
+  }
+
+  validEmail(email): boolean {
+    // eslint-disable-next-line
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  async handleSubmit(): Promise<void> {
+    if (!this.validEmail(this.form.email)) {
+      this.$refs.emailInput.focus();
+      return;
+    }
+
+    try {
+      await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({ 'form-name': 'signups', ...this.form }),
+      });
+
+      this.isSignedUp = true;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+</script>
